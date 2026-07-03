5ca2be287a3986b08b5901e6eb03d705d106cd78
Xue9702 <498289326@qq.com>
2026-07-03 23:05:52 +0800

    添加 express 框架，改成 Web 服务

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('你好，Mo-Home 正在运行');
+  res.send('你好, Mo-Home');
 });
 
 app.listen(port, () => {
-  console.log(`服务已启动：http://localhost:${port}`);
+  console.log(`服务已启动，访问端口: ${port}`);
 });
